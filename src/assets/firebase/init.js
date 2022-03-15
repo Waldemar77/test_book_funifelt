9d524fc1f2b760ab266984776215bc28ae4131fd
Waldemar77 <serggio719@gmail.com>
2022-03-15 17:00:38 -0500

    Configuration of firebase to allow the connection with github 2

--- src/assets/firebase/init.js
+++ src/assets/firebase/init.js
@@ -4,13 +4,13 @@ import firebase from "firebase"
 
 // This is the data about firebase connection. Please, don't change.
 const firebaseConfig = {
-  apiKey: "AIzaSyCTI3nV4cospvWRgOizEbeC0I1zPrs0hJM",
-  authDomain: "funifelt--ovas.firebaseapp.com",
-  projectId: "funifelt--ovas",
-  storageBucket: "funifelt--ovas.appspot.com",
-  messagingSenderId: "410224739568",
-  appId: "1:410224739568:web:491c4c753e435deb74ae35",
-  measurementId: "G-7SG7VZ3H4K"
+  apiKey: "AIzaSyDvBdsZZ7So8LNkm9GRgQKg6IV106feYi8",
+  authDomain: "viva-la-ciencia.firebaseapp.com",
+  projectId: "viva-la-ciencia",
+  storageBucket: "viva-la-ciencia.appspot.com",
+  messagingSenderId: "6600895630",
+  appId: "1:6600895630:web:e884f81f61b3c186392ddf",
+  measurementId: "G-D1YZYR6JJZ"
 };
 
 // Initializing Firebase
